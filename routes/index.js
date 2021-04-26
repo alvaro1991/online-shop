0ee74a7d30fbc9dfd3df7a34da088a10398de0ea
Álvaro <amauri@sonoc.io>
2021-04-26 18:22:49 +0200

    Implement authentication moddleware

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@
 const express = require('express')
 const app = require('../app')
 const productCtrl = require('../controllers/product')
+const auth = require('../middlewares/auth')
 const api = express.Router()
 
 //API Request examples
@@ -11,7 +12,9 @@ api.get("/product/:productId", productCtrl.getProduct);
 api.post("/product", productCtrl.saveProduct)
 api.put("/product/:productId", productCtrl.updateProduct)
 api.delete("/product/:productId", productCtrl.deleteProduct)
-
+api.get('/private', auth.isAuth, function(req, res){
+    res.status(200).send({message: 'Tienes acceso'})
+})
 
 module.exports = api 
 
