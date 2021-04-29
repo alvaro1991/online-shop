ca4d495f08c32b26fb336fe059450f796b05775f
Álvaro <amauri@sonoc.io>
2021-04-29 21:32:31 +0200

    Added sigIn function and refactor code

--- controllers/user.js
+++ controllers/user.js
@@ -21,7 +21,16 @@ function signUp(req, res) {
 }
 
 function signIn(req, res){
+    User.find({email: req.body.email}, (err, user)=>{
+        if(err) return res.status(500).send({message: err})
+        if(!user) return res.status(404).send({message: 'No existe el usuario'})
 
+        res.user = user
+        res.status(200).send({
+            message: 'Te has logeado correctamente',
+            token: service.createToken(user)
+        })
+    })
 
 }
 
