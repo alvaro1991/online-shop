cc9b9c8ed5defd72f39f764f0328dce2f14d1bf8
Álvaro <amauri@sonoc.io>
2021-04-30 17:46:51 +0200

    Added signIn and signUp functions

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,21 @@
 'use strict'
 
 const express = require('express')
-const app = require('../app')
+
 const productCtrl = require('../controllers/product')
+const userCtrl =  require('../controllers/user')
 const auth = require('../middlewares/auth')
+const user = require('../models/user')
 const api = express.Router()
 
 //API Request examples
 api.get("/product", productCtrl.getProducts)
 api.get("/product/:productId", productCtrl.getProduct);
-api.post("/product", productCtrl.saveProduct)
-api.put("/product/:productId", productCtrl.updateProduct)
-api.delete("/product/:productId", productCtrl.deleteProduct)
+api.post("/product", auth, productCtrl.saveProduct)
+api.put("/product/:productId", auth,  productCtrl.updateProduct)
+api.delete("/product/:productId", auth, productCtrl.deleteProduct)
+api.post('/signup', userCtrl.signUp)
+api.post('/signin', userCtrl.signIn)
 api.get('/private', auth, function(req, res){
     res.status(200).send({message: 'Tienes acceso'})
 })
