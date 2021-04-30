5d4aa0ae366a4d84301d32c2f41d57572e9755e8
Álvaro <amauri@sonoc.io>
2021-04-30 18:24:23 +0200

    Added views for product and login in hbs

--- app.js
+++ app.js
@@ -3,12 +3,21 @@
 const express = require("express");
 const app = express();
 const api = require('./routes')
+const hbs = require('express-handlebars')
 
 
 //Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use('/api', api)
+app.engine('.hbs', hbs({
+    defaultLayout: 'default',
+    extname: '.hbs'
+}))
+app.set('view engine', '.hbs')
 
+app.use('/api', api)
+app.get('/login', (req, res)=>{
+    res.render('login')
+})
 
 module.exports = app
